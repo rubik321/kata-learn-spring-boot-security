1489affd15ca4936c4e51ce4eb8c09a32c855bdf
Danil Pudovkin <dvpudovkin@gmail.com>
2022-08-16 11:18:57 +0500

    feat: add Role as @Entity, add @ManyToMay mapping to User

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -2,11 +2,30 @@ package ru.kata.spring.boot_security.demo.model;
 
 import org.springframework.security.core.GrantedAuthority;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
 public enum Role implements GrantedAuthority {
     USER, ADMIN;
 
+    @Id
+    private String name;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Set<User> users;
+
     @Override
     public String getAuthority() {
-        return null;
+        return name();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
