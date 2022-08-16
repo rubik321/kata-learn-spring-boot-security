b0795987401618f84c8e9e62f9cc6d7fffe97597
Danil Pudovkin <dvpudovkin@gmail.com>
2022-08-16 11:20:38 +0500

    feat: extend UserService with UserDetailsService

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -1,10 +1,11 @@
 package ru.kata.spring.boot_security.demo.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     void addUser(User user);
     User getUser(long id);
     List<User> getUserList();
