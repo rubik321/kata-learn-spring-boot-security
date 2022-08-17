2ca8bd9d5666046f050056c6074a738ca246d4ca
Danil Pudovkin <dvpudovkin@gmail.com>
2022-08-17 14:17:18 +0500

    fix: LazyInitializationException when fetching User
    Use enable_lazy_load_no_trans property instead of eager fetching in @ManyToMany of 'authorities' field of User.

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -29,7 +30,7 @@ public class User implements UserDetails {
     private String phoneNumber;
 
     @Column
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany
     @JoinTable(
             name = "users_roles",
             joinColumns = { @JoinColumn(name = "user_id") },
