2f4b7bb59dc4cf4c3bd31e64007c10460e7d5950
Danil Pudovkin <dvpudovkin@gmail.com>
2022-08-18 17:47:39 +0500

    feat: add RootController with redirection to /login

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RootController.java
@@ -0,0 +1,15 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+
+    @GetMapping
+    public String redirectToLogin() {
+        return "redirect:/login";
+    }
+}
