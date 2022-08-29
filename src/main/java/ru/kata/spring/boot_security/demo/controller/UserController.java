cc4dfb9888c981108d1a04624cddb722957a5c8b
Danil Pudovkin <dvpudovkin@gmail.com>
2022-08-29 15:06:19 +0500

    refactor(html): add allAuthorities attr to UserController::getUserPage

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -5,14 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.service.RoleService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    private final RoleService roleService;
+
+    public UserController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @GetMapping
-    public String getUserPrincipal(Model model) {
+    public String getUserPage(Model model) {
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        model.addAttribute("allAuthorities", roleService.getAllRoles());
         return "user";
     }
 }
