daff40d2542218dca6b7efdfb92c27deec6f0674
Danil Pudovkin <dvpudovkin@gmail.com>
2022-09-08 17:15:37 +0500

    refactor: return User JSON in AdminRestController::createUser

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
@@ -3,10 +3,12 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,12 +35,16 @@ public class AdminRestController {
     }
 
     @PostMapping()
-    public ResponseEntity<String> createUser(@RequestBody User user) {
-        try {
-            userService.addUser(user);
-            return ResponseEntity.ok("User has successful created");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error has occurred");
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.addUser(user);
+        if (createdUser == null) {
+            return ResponseEntity.badRequest().build();
+        } else {
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(createdUser.getId())
+                    .toUri();
+            return ResponseEntity.created(uri).body(createdUser);
         }
     }
 
