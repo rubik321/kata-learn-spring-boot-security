9c5a42fefca4055e2595aef4d2ce038379b64803
Danil Pudovkin <dvpudovkin@gmail.com>
2022-09-11 15:46:05 +0500

    refactor: redirect users with ADMIN & USER roles to /

--- src/main/java/ru/kata/spring/boot_security/demo/config/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/config/SuccessUserHandler.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 @Component
@@ -17,11 +19,7 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
                                         Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if (roles.contains("ROLE_ADMIN")) {
-            httpServletResponse.sendRedirect("/admin");
-        } else if (roles.contains("ROLE_USER")) {
-            httpServletResponse.sendRedirect("/user");
-        } else {
+        if (roles.containsAll(new HashSet<>(Arrays.asList("ROLE_ADMIN", "ROLE_USER")))) {
             httpServletResponse.sendRedirect("/");
         }
     }
